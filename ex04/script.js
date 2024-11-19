6804d431507186a1d9e2fc8af0a6677b501f96ba
BucksCompSci <bbennett@buchananschools.com>
2024-11-18 21:58:55 -0500

    Add ex04 materials

---
+++ ex04/script.js
@@ -0,0 +1,21 @@
+function task1() {
+  let myArray = [];
+  // This will run when you click the button.
+  myArray.forEach((item) => {
+    let el = document.createElement("p");
+    el.innerText = item;
+    target.appendChild(el);
+  });
+}
+
+function task2A() {
+  // Write your code in here
+}
+
+function task2B() {
+  // Writ your code in here
+}
+
+function task3() {
+  // Write your code in here
+}
