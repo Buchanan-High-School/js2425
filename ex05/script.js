abef92aeb9e7540b6e773fff61c455f85ac09bcf
BucksCompSci <bbennett@buchananschools.com>
2024-11-21 08:55:02 -0500

    add ex05 files

---
+++ ex05/script.js
@@ -0,0 +1,45 @@
+const person = {
+    "name": "Mr. Bennett",
+    "subject": "Chemistry",
+    "experience": 15
+}
+
+const teachers = [
+    {
+        "name": "Mrs. Frost",
+        "type": "Environmental Science",
+        "experience": 25
+    },
+    {
+        "name": "Mr. Cribley",
+        "subject": "Biology",
+        "experience": 15
+    },
+    {
+        "name": "Mrs. Lentz",
+        "subject": "Physical Science",
+        "experience": 22
+    },
+    {
+        "name": "Mr. Bennett",
+        "subject": "Chemistry",
+        "experience": 15
+    },
+]
+
+function task1() {
+    // Create your own object assigned to a variable.
+    // console.log your object when this function is run.
+}
+
+function task2() {
+    // Write some HTML to create a card display for a person.
+    // When you click the button, the data from the `person`
+    // object above should be filled in on the page.
+}
+
+function task3() {
+    // 1. Use the array of teachers to start a loop.
+    // 2. In the loop, add a "role" property to each object as they go through the loop
+    // 3. Use a template string to add a sentence about each person into the #task3 container.
+}
